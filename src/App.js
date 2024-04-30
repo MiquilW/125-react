0f7b51e953e615392f61621dc7c1024c0d84b472
MiquilW <michael.welsome@gmail.com>
2024-04-29 22:06:51 -0700

    init

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Michael Welsome
+        </h1>
+        <h3>
+          Software Developer
+        </h3>
+        <a
+          className="App-link"
+          href="https://github.com/MiquilW"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
